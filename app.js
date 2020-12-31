8d1e0d7cddbb580e06c7bcd10fb2c1885b6f6bc5
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-12-31 18:56:21 -0500

    added template literals with data pulled from inquirer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 //I want to generate a webpage that displays my team's basic info
-const managerQuestions = () =>
+const questions = () =>
   inquirer.prompt([
     {
       type: "input",
@@ -34,25 +34,62 @@ const managerQuestions = () =>
                     type: "input",
                     name: "office",
                     message: "What is the office number for the manager?",
-                }).then((managerData)) => {
-                    
+                }).then((managerData) => {
+                    const managerProfile = fs.appendFile(
+                        `<div class="card" style="width: 18rem;">
+                        <img src="https://www.shutterstock.com/image-vector/online-training-process-icon-flat-style-1176599782" class="card-img-top" alt="Icon for directory">
+                        <div class="card-body">
+                          <h5 class="card-title">${data.name}</h5>
+                          <p class="card-text">${data.role}</p>
+                          <p class="card-text">${data.email}</p>
+                          <p class="card-text">${managerData.officeNumber}</p>
+                        </div>
+                      </div>`
+                    );
                 }
-        }
+                );
+        
         if (data.role == "engineer"){
                 inquirer.prompt({
                     type: "input",
                     name: "github",
                     message: "What is the GitHub profile name for the engineer?",
-                }).then((engineerData)) =>
+                }).then((engineerData) => {
+                    const engineerProfile = fs.appendFile(
+                        `<div class="card" style="width: 18rem;">
+                        <img src="https://www.shutterstock.com/image-vector/online-training-process-icon-flat-style-1176599782" class="card-img-top" alt="Icon for directory">
+                        <div class="card-body">
+                          <h5 class="card-title">${data.name}</h5>
+                          <p class="card-text">${data.role}</p>
+                          <p class="card-text">${data.email}</p>
+                          <p class="card-text">${engineerData.github}</p>
+                        </div>
+                      </div>`
+                    );
+                }
 
-        }
+                )
+            }
         if (data.role == "intern"){
             inquirer.prompt({
                 type: "input",
                 name: "school",
                 message: "Which school does the intern attend?"
-            }).then((internData)) => 
+            }).then((internData) => {
+                const internProfile = fs.appendFile(
+                    `<div class="card" style="width: 18rem;">
+                    <img src="https://www.shutterstock.com/image-vector/online-training-process-icon-flat-style-1176599782" class="card-img-top" alt="Icon for directory">
+                    <div class="card-body">
+                      <h5 class="card-title">${data.name}</h5>
+                      <p class="card-text">${data.role}</p>
+                      <p class="card-text">${data.email}</p>
+                      <p class="card-text">${internData.school}</p>
+                    </div>
+                  </div>`
+                );
+            }
+            )
 
-        }
+        };
 
 
